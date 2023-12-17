780f925e6519221d75c5b2e093b4516718d5efa8
MaxFireFox <99403419+MaxFireFox@users.noreply.github.com>
2023-12-17 20:41:13 +0300

    task for medgarant

--- task_for_medgarant/meeting.py
+++ task_for_medgarant/meeting.py
@@ -1,6 +1,21 @@
+"""
+    This class has objects start and stop -- string depicting start and finish of the meeting
+
+    It also has methods:
+    * check_in -- whether certain time is inside the time interval of the meeting or not
+    * dict -- returns dictionary of objects of this class instance
+"""
+
+
 class Meeting:
 
-    def __init__(self, start):
+    def __init__(self, start: str):
+        """
+        Gets str format start time of the meeting as input, creates stop time according to the task
+        (30 min later), also in str format
+        :param start: string for meeting start time
+        """
+
         self.start = start
         self.stop = start
         if self.stop[3] >= "3":
@@ -10,10 +25,22 @@ class Meeting:
         else:
             self.stop = self.stop[:3] + str(int(self.stop[3]) + 3) + self.stop[4]
 
-    def check_in(self, time):
+    def check_in(self, time: str):
+        """
+        Checks whether certain time is inside the time interval of the meeting or not
+        :param time: string for the time we check
+        :return: boolean answer
+        :rtype bool
+        """
+
         return self.start <= time < self.stop
 
     def dict(self):
+        """
+        Returns dictionary of objects of this class instance
+        :return: dictionary of start and stop time
+        :rtype dict
+        """
         return {
             'start': self.start,
             'stop': self.stop
